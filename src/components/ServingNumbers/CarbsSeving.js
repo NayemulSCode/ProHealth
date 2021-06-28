c1ed714f259561ad2b1ac0b19e6d4db2e32b4c49
mahmudsagar <mahmudsagar9@gmail.com>
2021-06-29 02:29:13 +0600

    servings

--- src/components/ServingNumbers/CarbsSeving.js
+++ src/components/ServingNumbers/CarbsSeving.js
@@ -1,31 +1,40 @@
 import React, { useState } from "react";
+import { AnthropContext, useGlobalContext } from "../../context/Context";
 
 const CarbsSeving = () => {
+    const {tcalorie} = useGlobalContext(AnthropContext);
     const [refs]=useState({
-        tCalorie: React.createRef(),
-        grams:React.createRef(),
-        tCarbs:React.createRef(),
-        pOfCals:React.createRef(),
+        starchyVegies: React.createRef(),
+        fruit:React.createRef(),
+        beans:React.createRef(),
+        grains:React.createRef(),
+        otherCarb:React.createRef(),
+        dairy:React.createRef(),
+        nonStarchyVegies:React.createRef(),
     })
     const [vars] = useState({
-        tCalorie: 0,
-        grams: 0,
-        tCarbs: 0,
-        pOfCals: 0,
+        starchyVegies: 0,
+        fruit: 0,
+        beans: 0,
+        grains: 0,
+        otherCarb: 0,
+        dairy: 0,
+        nonStarchyVegies: 0,
     });
     const handleSubmit =(e)=>{
         e.preventDefault();
-    
-    
     }
     const onChange=(e)=>{
         // detect value from input filed form data
         const {name, value} = e.target;
         vars[name] = value;
-        const tCalorie = parseFloat(refs.tCalorie.current.value).toFixed(2);
-        const grams = parseFloat(refs.grams.current.value).toFixed(2);
-        const tCarbs = parseFloat(refs.tCarbs.current.value).toFixed(2);
-        const pOfCals = parseFloat(refs.pOfCals.current.value).toFixed(2);
+        const starchyVegies = parseFloat(refs.starchyVegies.current.value).toFixed(2);
+        const fruit = parseFloat(refs.fruit.current.value).toFixed(2);
+        const beans = parseFloat(refs.beans.current.value).toFixed(2);
+        const grains = parseFloat(refs.grains.current.value).toFixed(2);
+        const otherCarb = parseFloat(refs.otherCarb.current.value).toFixed(2);
+        const dairy = parseFloat(refs.dairy.current.value).toFixed(2);
+        const nonStarchyVegies = parseFloat(refs.nonStarchyVegies.current.value).toFixed(2);
         
     // value assign inot input filed after calculating
         if (name === 'tCarbs') refs.tCarbs.current.value = grams * weights;
@@ -35,83 +44,90 @@ const CarbsSeving = () => {
     
     }
     return (
-        <div>
-            <form className="form" onSubmit={handleSubmit}>
-                <table className="table-fill">
-                    <thead>
-                        <tr>
-                            <th>Total CRABS</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <label htmlFor="tCalorie">
-                                    Daily Calorie Target{" "}
-                                </label>
-                            </td>
-                            <td>
-                                <input
-                                    onChange={onChange}
-                                    defaultValue={vars.tCalorie}
-                                    ref={refs.tCalorie}
-                                    name="tCalorie"
-                                    id="tCalorie"
-                                    type="text"
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="grams">grams/kg</label>
-                            </td>
-                            <td>
-                                <input
-                                    onChange={onChange}
-                                    defaultValue={vars.grams}
-                                    ref={refs.grams}
-                                    name="grams"
-                                    id="grams"
-                                    type="text"
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="tCarbs">total gm carbs</label>
-                            </td>
-                            <td>
-                                <input
-                                    onClick={onChange}
-                                    defaultValue={vars.tCarbs}
-                                    ref={refs.tCarbs}
-                                    name="tCarbs"
-                                    id="tCarbs"
-                                    type="number"
-                                    readOnly
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="pOfCals">% of cals</label>
-                            </td>
-                            <td>
-                                <input
-                                    onClick={onChange}
-                                    defaultValue={vars.pOfCals}
-                                    ref={refs.pOfCals}
-                                    id="pOfCals"
-                                    name="pOfCals"
-                                    type="number"
-                                    readOnly
-                                />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </form>
+<div className="calulator-table" id="tracker">
+        <form className="form" onSubmit={handleSubmit}>
+            <table className="table-fill">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Scoops/Cups</th>
+                        <th>Calories</th>
+                        <th>Carbs</th>
+                        <th>Protein</th>
+                        <th>Fat</th>
+                        <th>Fiber</th>
+                    </tr>
+                </thead>
+                <tbody> 
+                    <tr>
+                        <td><label htmlFor="grams">UCAN</label></td>
+                        <td>
+                            <input onKeyUpCapture={(e)=>setUcan(e.target.value)} name='grams' id="grams" type="text"/>
+                        </td>
+                        <td id="calorie">{ucan*120}</td>
+                        <td>{28*ucan}</td>
+                        <td>{270*ucan}</td>
+                        <td>{0*ucan}</td>
+                        <td>{1*ucan}</td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="grams">Vega Protein & Greens</label></td>
+                        <td>
+                            <input onKeyUpCapture={(e)=>setVega(e.target.value)} name='grams' id="grams" type="text"/>
+                        </td>
+                        <td id="calorie">{110*vega}</td>
+                        <td>{2*vega}</td>
+                        <td>{20*vega}</td>
+                        <td>{2*vega}</td>
+                        <td>{1*vega}</td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="grams">Vital Nutrients ProWhey (2 tbsp)</label></td>
+                        <td>
+                            <input onKeyUpCapture={(e)=>setVital(e.target.value)} name='grams' id="grams" type="text"/>
+                        </td>
+                        <td>{95*vital}</td>
+                        <td>{0*vital}</td>
+                        <td>{23*vital}</td>
+                        <td>{0*vital}</td>
+                        <td>{0*vital}</td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="grams">GOL Whey Protein</label></td>
+                        <td>
+                            <input onKeyUpCapture={(e)=>setWhey(e.target.value)} name='grams' id="grams" type="text"/>
+                        </td>
+                        <td>{120*whey}</td>
+                        <td>{6*whey}</td>
+                        <td>{24*whey}</td>
+                        <td>{0.5*whey}</td>
+                        <td>{2*whey}</td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="grams">GOL Plant Protein</label></td>
+                        <td>
+                            <input onKeyUpCapture={(e)=>setPlant(e.target.value)} name='grams' id="grams" type="text"/>
+                        </td>
+                        <td>{110*plant}</td>
+                        <td>{5.5*plant}</td>
+                        <td>{15*plant}</td>
+                        <td>{2*plant}</td>
+                        <td>{4.5*plant}</td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="grams">GOL Whey Protein</label></td>
+                        <td>
+                            <input onKeyUpCapture={(e)=>setWhey(e.target.value)} name='grams' id="grams" type="text"/>
+                        </td>
+                        <td>{120*whey}</td>
+                        <td>{6*whey}</td>
+                        <td>{24*whey}</td>
+                        <td>{0.5*whey}</td>
+                        <td>{2*whey}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </form> 
         </div>
     );
 };
