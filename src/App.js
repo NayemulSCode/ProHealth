ea4625b7830061cf5030e3ba636db0f3e02a99b0
Nayemul <nayemulhasan2016@gmail.com>
2021-07-01 20:20:27 +0600

    add multisteps from navigator

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import Energy from './components/Estimated/Energy';
 import Protein from './components/Estimated/Protein';
 import Recommendation from './components/Estimated/Recommendation';
 import ServingNumbers from './components/ServingNumbers/ServingNumbers';
+
+import MultiStep from 'react-multistep'
+
+ const steps = [
+  { name:'Anthropometrics', component: <Anthropometrics /> },
+  { name:'Energy', component: <Energy /> },
+  { name:'Protein', component: <Protein /> },
+  { name:'Recommendation', component: <Recommendation /> },
+  { name:'Serving', component: <ServingNumbers /> },
+]
+const prevStyle = {'background': '#33c3f0', 'border-width': '2px', 'padding':'10px'}
+const nextStyle = {'background': '#33c3f0',  'border-width': '2px', 'padding':'10px', 'margin-left':'5px'}
 function App() {
   return (
     <div className="App">
-      <h1 className="">Health app</h1>
-      <Anthropometrics />
+      <MultiStep showNavigation={true} steps={steps} prevStyle={prevStyle} nextStyle={nextStyle} />
+      {/* <Anthropometrics />
       <EnergyNeed />
       <Energy />
       <Protein />
       <Recommendation />
-      <ServingNumbers />
+      <ServingNumbers /> */}
     </div>
   );
 }
